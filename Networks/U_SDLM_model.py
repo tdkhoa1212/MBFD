7cbebdd11caa2603f0be20eac668eb2725f85da4
tdkhoa1212 <tdkhoa1212@gmail.com>
2023-01-27 07:01:10 +0700

    a

--- Networks/U_SDLM_model.py
+++ Networks/U_SDLM_model.py
@@ -3,44 +3,24 @@ from tensorflow.keras.layers import BatchNormalization, Dropout, Activation, Lam
 from tensorflow.keras import regularizers
 
 
-# def U_SDLM(input, opt):
-#   x = Dense(opt.e_input_shape*4,
-#             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-#             bias_regularizer=regularizers.l2(1e-4),
-#             activity_regularizer=regularizers.l2(1e-5))(input)
-#   x = Dense(opt.e_input_shape*2,
-#             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-#             bias_regularizer=regularizers.l2(1e-4),
-#             activity_regularizer=regularizers.l2(1e-5))(x)
-#   x = Dense(opt.e_input_shape//2,
-#             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-#             bias_regularizer=regularizers.l2(1e-4),
-#             activity_regularizer=regularizers.l2(1e-5))(x)
-#   x = Dense(opt.e_input_shape*2,
-#             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-#             bias_regularizer=regularizers.l2(1e-4),
-#             activity_regularizer=regularizers.l2(1e-5))(x)
-#   x = Dense(opt.e_input_shape*4,
-#             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-#             bias_regularizer=regularizers.l2(1e-4),
-#             activity_regularizer=regularizers.l2(1e-5))(x)
-#   x = Dropout(rate=0.5)(x)
-#   x = Dense(opt.embedding_size)(x)
-#   x = BatchNormalization()(x)
-#   x = Activation('relu')(x)
-#   x = Lambda(lambda  x: K.l2_normalize(x, axis=1))(x)
-#   return x
-
 def U_SDLM(input, opt):
-  x = Dense(opt.e_input_shape,
+  x = Dense(opt.embedding_size*4,
             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
             bias_regularizer=regularizers.l2(1e-4),
             activity_regularizer=regularizers.l2(1e-5))(input)
-  x = Dense(opt.e_input_shape//2,
+  x = Dense(opt.embedding_size*2,
             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
             bias_regularizer=regularizers.l2(1e-4),
             activity_regularizer=regularizers.l2(1e-5))(x)
-  x = Dense(opt.e_input_shape//4,
+  x = Dense(opt.embedding_size//2,
+            kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+            bias_regularizer=regularizers.l2(1e-4),
+            activity_regularizer=regularizers.l2(1e-5))(x)
+  x = Dense(opt.embedding_size*2,
+            kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+            bias_regularizer=regularizers.l2(1e-4),
+            activity_regularizer=regularizers.l2(1e-5))(x)
+  x = Dense(opt.embedding_size*4,
             kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
             bias_regularizer=regularizers.l2(1e-4),
             activity_regularizer=regularizers.l2(1e-5))(x)
@@ -50,3 +30,4 @@ def U_SDLM(input, opt):
   x = Activation('relu')(x)
   x = Lambda(lambda  x: K.l2_normalize(x, axis=1))(x)
   return x
+
