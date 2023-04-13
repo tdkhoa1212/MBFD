df46dc420e8e192675c6b188030022df3b86ed44
tdkhoa1212 <tdkhoa1212@gmail.com>
2023-04-13 18:26:53 +0700

    a

--- train.py
+++ train.py
@@ -9,14 +9,14 @@ def parse_opt(known=False):
     parser.add_argument('--ML_method', default=['RF', 'KNN', 'LGBM', 'euclidean', 'cosine', 'SVM']  , type=list, help='SVM, RF, KNN, LGBM, euclidean, cosine')
     parser.add_argument('--scaler', default="PowerTransformer", type=str, help='MinMaxScaler, MaxAbsScaler, StandardScaler, RobustScaler, Normalizer, QuantileTransformer, PowerTransformer')
     parser.add_argument('--type_data', type=str, default='vibration', help='vibration, MCS1, MCS2')
-    parser.add_argument('--data_dir', type=str, default='./data/PU_data', help='direction of data')
-    parser.add_argument('--weights_path', type=str, default='./results', help='direction of data')
-    parser.add_argument('--img_outdir', type=str, default='./results/images', help='direction of data')
+    parser.add_argument('--data_dir', type=str, default='/content/drive/MyDrive/Khoa/data/PU_data', help='direction of data')
+    parser.add_argument('--weights_path', type=str, default='/content/drive/MyDrive/Khoa/results', help='direction of data')
+    parser.add_argument('--img_outdir', type=str, default='/content/drive/MyDrive/Khoa/results/images', help='direction of data')
     parser.add_argument('--load_weights', default=True, type=bool)
     parser.add_argument('--Ex_feature', type=str, default='fre', help='time, fre, time_fre')
     parser.add_argument('--PU_table_8', default=False, type=bool)
     parser.add_argument('--PU_table_10', default=True, type=bool)
-    parser.add_argument('--path_saved_data', type=str, default='./results', help='direction of data')
+    parser.add_argument('--path_saved_data', type=str, default='/content/drive/MyDrive/Khoa/results', help='direction of data')
 
     # Parameters--------
     parser.add_argument('--alpha', default=0.4, type=int)
